6b38ce780bc544415e4db2be73ad7900e7d185a5
3m5.Stefan Beyer <Stefan.Beyer@3m5.de>
2017-11-11 16:00:27 +0100

    prototype movement

---
+++ New Unity Project/Assets/Scripts/Movement/PathMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMovement : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		iTween.MoveTo(gameObject, new Hashtable
+		{
+			{"path", iTweenPath.GetPath("path")},
+			{"time", 30},
+		});
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
