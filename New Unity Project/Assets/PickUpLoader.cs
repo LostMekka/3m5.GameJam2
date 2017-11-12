e1bd1e3c23ec91291adcc37fa481fd36eace3442
Lars Wobus <lars.wobus@web.de>
2017-11-12 17:31:22 +0100

    ADD pickup creation

---
+++ New Unity Project/Assets/PickUpLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpLoader : MonoBehaviour {
+
+    public string[] _prefabNames = new string[] { "DummyPickup", "InvertControls", "LowerSpeed", "SpeedUp" };
+    public int count = 5;
+    public Vector3 pickupScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+    Pickup[] _placeholders;
+
+    void Start () {
+        _placeholders = GetComponentsInChildren<Pickup>();
+        for (var index = 0; index < count; ++index)
+        {
+            CreateRandomItemAtRandomPosition();
+        }
+
+        DestroyPlaceHolder();
+    }
+
+    void CreateRandomItemAtRandomPosition()
+    {
+        var pickUpIndex = Random.Range(0, _placeholders.Length);
+
+        var prefabName = SelectRandomPrefabName();
+        Debug.Log(prefabName);
+        GameObject pickUp = Instantiate(Resources.Load(prefabName), Vector3.zero, Quaternion.identity) as GameObject;
+        pickUp.transform.parent = this.transform;
+        pickUp.transform.position = _placeholders[pickUpIndex].transform.position;
+        pickUp.transform.localScale = pickupScale;
+    }
+
+    string SelectRandomPrefabName()
+    {
+        var index = Random.Range(0, _prefabNames.Length);
+        return _prefabNames[index];
+    }
+
+    void DestroyPlaceHolder ()
+    {
+        for (var index = 0; index < _placeholders.Length; ++index)
+        {
+            Destroy(_placeholders[index].gameObject);
+        }
+    }
+}
